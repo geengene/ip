f9fe1e6a400c134f980fe5c43b74eabac1ac8be3
Kuah Gene Qhee <132677007+geengene@users.noreply.github.com>
2026-09-04 07:19:09 +0800

    Use varargs to centralize UI message formatting

--- src/main/java/duke/ui/Ui.java
+++ src/main/java/duke/ui/Ui.java
@@ -15,29 +15,24 @@ public class Ui {
      * Prints the opening chatbot message.
      */
     public void showGreeting() {
-        System.out.println(LINE);
-        System.out.println(CHATBOT_BANNER);
-        System.out.println("Hello! I'm " + CHATBOT_NAME + ".");
-        System.out.println("What can I do for you?");
-        System.out.println(LINE);
+        showMessage(
+                CHATBOT_BANNER,
+                "Hello! I'm " + CHATBOT_NAME + ".",
+                "What can I do for you?");
     }
 
     /**
      * Prints the closing chatbot message.
      */
     public void showGoodbye() {
-        System.out.println(LINE);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(LINE);
+        showMessage("Bye. Hope to see you again soon!");
     }
 
     /**
      * Prints an error message caused by invalid user input or storage problems.
      */
     public void showError(String message) {
-        System.out.println(LINE);
-        System.out.println("OOPS!!! " + message);
-        System.out.println(LINE);
+        showMessage("OOPS!!! " + message);
     }
 
     /**
@@ -47,11 +42,10 @@ public class Ui {
      * @param taskCount Number of tasks after adding the task.
      */
     public void showTaskAdded(Task task, int taskCount) {
-        System.out.println(LINE);
-        System.out.println("Got it. I've added this task:");
-        System.out.println("  " + task);
-        System.out.println("Now you have " + taskCount + " tasks in the list.");
-        System.out.println(LINE);
+        showMessage(
+                "Got it. I've added this task:",
+                "  " + task,
+                "Now you have " + taskCount + " tasks in the list.");
     }
 
     /**
@@ -61,31 +55,28 @@ public class Ui {
      * @param taskCount Number of tasks after deleting the task.
      */
     public void showTaskDeleted(Task deletedTask, int taskCount) {
-        System.out.println(LINE);
-        System.out.println("Noted. I've removed this task:");
-        System.out.println("  " + deletedTask);
-        System.out.println("Now you have " + taskCount + " tasks in the list.");
-        System.out.println(LINE);
+        showMessage(
+                "Noted. I've removed this task:",
+                "  " + deletedTask,
+                "Now you have " + taskCount + " tasks in the list.");
     }
 
     /**
      * Prints a confirmation after marking a task as done.
      */
     public void showTaskMarked(Task task) {
-        System.out.println(LINE);
-        System.out.println("Nice! I've marked this task as done:");
-        System.out.println("  " + task);
-        System.out.println(LINE);
+        showMessage(
+                "Nice! I've marked this task as done:",
+                "  " + task);
     }
 
     /**
      * Prints a confirmation after marking a task as not done.
      */
     public void showTaskUnmarked(Task task) {
-        System.out.println(LINE);
-        System.out.println("OK, I've marked this task as not done yet:");
-        System.out.println("  " + task);
-        System.out.println(LINE);
+        showMessage(
+                "OK, I've marked this task as not done yet:",
+                "  " + task);
     }
 
     /**
@@ -108,6 +99,19 @@ public class Ui {
         System.out.println("Here are the matching tasks in your list:");
         for (int i = 0; i < tasks.size(); i++) {
             System.out.println((i + 1) + "." + tasks.get(i));
+        }
+        System.out.println(LINE);
+    }
+
+    /**
+     * Prints any number of message lines between the standard chatbot separators.
+     *
+     * @param lines Message lines to print in order.
+     */
+    private void showMessage(String... lines) {
+        System.out.println(LINE);
+        for (String line : lines) {
+            System.out.println(line);
         }
         System.out.println(LINE);
     }
