26ce0ed3ff0aaa9229b39b4edb5c68c8a05a478d
Kuah Gene Qhee <132677007+geengene@users.noreply.github.com>
2026-09-04 06:56:16 +0800

    Add Checkstyle configuration
    Configure Gradle Checkstyle using the SE-EDU rules so style checks can be run with checkstyleMain and checkstyleTest.

--- src/main/java/duke/Duke.java
+++ src/main/java/duke/Duke.java
@@ -1,13 +1,13 @@
 package duke;
 
+import java.util.Scanner;
+
 import duke.exception.DukeException;
 import duke.parser.Parser;
 import duke.storage.Storage;
 import duke.task.Task;
 import duke.task.TaskList;
 import duke.ui.Ui;
-
-import java.util.Scanner;
 
 /**
  * Entry point for the geen chatbot.
